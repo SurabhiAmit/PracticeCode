1c58adf8a8d721b3a0f0ee8bed8e63f4bbdd3a68
surabhi <surabhivasudev1993@gmail.com>
2017-07-10 11:55:30 -0500

    splitting_string_into_words
    Define a procedure, split_string, that takes two inputs: the string to split and a string containing all of the characters considered separators. The procedure should return a list of strings that break the source string up by the characters in the splitlist.

---
+++ splitting_a_string_into_component_words.py
@@ -0,0 +1,37 @@
+# Define a procedure, split_string, that takes two
+# inputs: the string to split and a string containing
+# all of the characters considered separators. The
+# procedure should return a list of strings that break
+# the source string up by the characters in the
+# splitlist.
+
+
+    
+def split_string(source,splitlist):
+    output=[]
+    atsplit=True
+    for char in source:
+        if char in splitlist:
+            atsplit=True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit=False
+            else:
+                output[-1]=output[-1]+char
+                
+    return output
+            
+                  
+
+out = split_string("This is a test-of the,string separation-code!"," ,!-")
+print out
+#>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
+
+out = split_string("After  the flood   ...  all the colors came out.", " .")
+print out
+#>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
+
+out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+print out
+#>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
