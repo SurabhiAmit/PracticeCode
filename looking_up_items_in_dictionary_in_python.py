b26ce8ed7e3ba16feab2d7bfdf92864004632d24
surabhi <surabhivasudev1993@gmail.com>
2017-07-13 11:30:16 -0500

    looking_up_items_in_dictionary_in_python
    Define a procedure, is_offered(courses, course, hexamester), that returns
    True if the input course is offered in the input hexamester(dictionary), and returns False otherwise.

---
+++ looking_up_items_in_dictionary_in_python.py
@@ -0,0 +1,75 @@
+courses = {
+    'feb2012': { 'cs101': {'name': 'Building a Search Engine',
+                           'teacher': 'Dave',
+                           'assistant': 'Peter C.'},
+                 'cs373': {'name': 'Programming a Robotic Car',
+                           'teacher': 'Sebastian',
+                           'assistant': 'Andy'}},
+    'apr2012': { 'cs101': {'name': 'Building a Search Engine',
+                           'teacher': 'Dave',
+                           'assistant': 'Sarah'},
+                 'cs212': {'name': 'The Design of Computer Programs',
+                           'teacher': 'Peter N.',
+                           'assistant': 'Andy',
+                           'prereq': 'cs101'},
+                 'cs253': 
+                {'name': 'Web Application Engineering - Building a Blog',
+                           'teacher': 'Steve',
+                           'prereq': 'cs101'},
+                 'cs262': 
+                {'name': 'Programming Languages - Building a Web Browser',
+                           'teacher': 'Wes',
+                           'assistant': 'Peter C.',
+                           'prereq': 'cs101'},
+                 'cs373': {'name': 'Programming a Robotic Car',
+                           'teacher': 'Sebastian'},
+                 'cs387': {'name': 'Applied Cryptography',
+                           'teacher': 'Dave'}},
+    'jan2044': { 'cs001': {'name': 'Building a Quantum Holodeck',
+                           'teacher': 'Dorina'},
+                 'cs003': {'name': 'Programming a Robotic Robotics Teacher',
+                           'teacher': 'Jasper'},
+                     }
+    }
+
+# If you want to loop through the keys in the dictionary,
+# you can use the construct below.
+#         for <key> in <dictionary>:
+#                    <block>  
+# For example, this procedure returns a list of all the courses offered 
+# in the given hexamester:
+
+def courses_offered(courses, hexamester):
+    res = []
+    for c in courses[hexamester]:
+        res.append(c)
+    return res
+
+# Define a procedure, is_offered(courses, course, hexamester), that returns 
+# True if the input course is offered in the input hexamester, and returns 
+# False otherwise. 
+ 
+ Initial code I wrote:
+ 
+# def is_offered(courses, course, hexamester):
+#    if course in courses_offered(courses,hexamester):
+#        return True
+#    return False
+
+Code improved after re-visiting it:
+
+def is_offered(courses, course, hexamester):
+    return course in courses_offered(courses, hexamester)
+
+
+print is_offered(courses, 'cs101', 'apr2012')
+#>>> True
+
+print is_offered(courses, 'cs003', 'apr2012')
+#>>> False
+print is_offered(courses, 'cs001', 'jan2044')
+#>>> True
+
+print is_offered(courses, 'cs253', 'feb2012')
+#>>> False
+    
