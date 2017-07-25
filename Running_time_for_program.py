bbb4d38fc7c960e70cecf71f08518ef8bd2d9ea3
surabhi <surabhivasudev1993@gmail.com>
2017-07-25 16:49:28 -0500

    Running_time_for_program

---
+++ Running_time_for_program.py
@@ -0,0 +1,19 @@
+# counting steps in naive as a function of a
+
+def naive(a, b):
+    x = a
+    y = b
+    z = 0
+    while x > 0:
+        z = z + y
+        x = x - 1
+    return z
+
+def time(a):
+    # The number of steps it takes to execute naive(a, b)
+    # as a function of a
+    return 3+2*a
+    
+# return and while or for statements does not take time
+#(only body of loop takes time),if and other statements take time    
+    
